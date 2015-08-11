2bfe6072d786d96884507d2729458249ce23348e
Fermoto5HD <Fermoto5HD@outlook.com>
2015-08-10 22:44:42 -0300

    [Alpha 2] Retoques generales

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Web-v1.0 (alpha)
+# Web-v1.0 (Alpha 2)
 Repositorio de la versión alpha del próximo sitio del portal de tecnología "Lantalks". 
+
+#Cambios realizados
+## Alpha 2
+- Agregado el soporte responsive. 
+- Cambios en el layout y disposición de las partes del portal. 
+- Retoques en las animaciones via jQuery. 
+- Incorporado el borrado de caché al cargar las páginas mediante jQuery. 
+
+## Alpha 1
+- Primer commit del sitio. 
--- js/ux.js
+++ js/ux.js
@@ -1,11 +1,19 @@
+$.ajaxSetup ({
+	// Disable caching of AJAX responses
+	cache: false
+});
 $(window).load(function(){
-	$("header").css("height", "0px");
+	$("#contenido-pagina").load("inicio.html");
+
+	$("header").css("height", "0px").css("z-index", "3");
 	$("header #logotipo").css("padding-top", "0px").css("opacity", "0");
 	$("header nav ul").css("padding-left", "0px").css("opacity", "0");
 
 	$("section#destacado").css("opacity", "0");
-	$("section#recientes").css("opacity", "0");
+	$("section#mas-leidos").css("opacity", "0");
+	$("section#mas-leidos").css("width", "0");
 	$("section#contenido-principal").css("opacity", "0");
+
 
 	//$("section#recientes").css("width", "0px");
 
@@ -13,10 +21,9 @@ $(window).load(function(){
 	$("header").animate({height: $("header").get(0).scrollHeight}, 300, function(){
 		$("#logotipo").animate({"padding-top": "10px", opacity: "1"}, 300, function(){
 			$("ul").animate({"padding-left": "10px", opacity: "1", "transition-duration": "0.5s"}, 300);
-			$("section#destacado").animate({opacity: "1"}, 300, function(){
-				$("section#recientes").animate({opacity: "1"}, 300);
-				$("section#contenido-principal").animate({opacity: "1"}, 300);
-			});
+				$("section#destacado").animate({opacity: "1"}, 300, function(){
+					$("section#contenido-principal").animate({opacity: "1"}, 300);
+				});
 		});
 	});
 });
